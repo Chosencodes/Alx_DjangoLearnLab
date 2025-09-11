856012066fa4975c56dde1f62e3889c52639e6cd
Chosencodes <otaborchoosen1@gmail.com>
2025-09-11 03:00:26 +0100

    Add UserProfile model and role-based views/templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView   # 👈 exact line for checker
 from .models import Book
 from .models import Library
+from django.contrib.auth.decorators import user_passes_test
+
 
 
 # Function-based view to list all books
@@ -46,3 +48,23 @@ from django.http import HttpResponse
 def home_view(request):
     return render(request, 'relationship_app/home.html')
 
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
