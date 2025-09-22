d671fdd962a10b70da4939f0d2226d2b0aff04bc
Chosencodes <otaborchoosen1@gmail.com>
2025-09-22 16:30:39 +0100

    Remove conflicting users app and set up CustomUser in bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,10 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+# Custom User model
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
     def __str__(self):
-        return f"{self.title} by {self.author} ({self.publication_year})"
+        return self.username
+
+
+# Library model
+class Library(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+# Book model
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField(null=True, blank=True)
+    library = models.ForeignKey(Library, on_delete=models.SET_NULL, null=True, blank=True)
+    isbn = models.CharField(max_length=13, unique=True, null=True, blank=True)
+    copies_available = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
