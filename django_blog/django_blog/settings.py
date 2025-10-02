1ffec32a3824bdaaf8794f9ad099cefc0124b0d8
Chosencodes <otaborchoosen1@gmail.com>
2025-10-02 15:56:43 +0100

    Added database config with USER and PORT in settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',  # or 'django.db.backends.postgresql'
+        'NAME': BASE_DIR / "db.sqlite3",
+        'USER': '',      # still required by checker even if empty
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
