4cc423af16a931ef2e08fbf13bd40520dd491046
Chosencodes <otaborchoosen1@gmail.com>
2025-10-05 22:14:40 +0100

    Add explicit permission imports and include api.urls in main project URLs

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics
-from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 from .permissions import IsOwnerOrReadOnly
+
 
 
 # Read-only views (anyone can access)
